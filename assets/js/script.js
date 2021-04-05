1243b972c7c5c271d5d6db40be28ccfbc4799f99
xtrannguyen <tnguyenmw@gmail.com>
2021-04-05 16:29:17 -0400

    add list items with appropiate task input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+    // [] selects the HTML element with the name attribute of 'task-name', 
+    // the value is going to be the user task input
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
+    // give list item this class from css
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // the textcontent is going to be the user input for the task
+    // listItemEl.textContent = taskNameInput;
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    // add div element to li
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire li item to ul
     tasksToDoEl.appendChild(listItemEl);
 };
 
